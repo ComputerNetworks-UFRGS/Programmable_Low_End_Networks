066ed4c1625d3ffaf36b8b5e3e87bc509b63bc7a
Marcelo Basso <marcelo.basso@inf.ufrgs.br>
2024-06-25 14:36:35 -0300

    ✨ updates automation script
    Co-authored-by: Julien <julien-glmt@users.noreply.github.com>

--- p4pi-files/run_prototype.sh
+++ p4pi-files/run_prototype.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+
 ROOT_USER=root
 ROOT_PASS=raspberry
-NAME_P4_MIDDLE=p4pi-middle
-NAME_P4_END=p4pi-end
+SSID_P4_MIDDLE=p4pi-middle
+SSID_P4_END=p4pi-end
 IP_P4_MIDDLE=192.168.4.2
 IP_P4_END=192.168.4.1
 INTERNET=http://google.com
@@ -19,24 +20,66 @@ function is_online() {
     echo $?
 }
 
-sudo -i
-${ADM_PASS}
+function run_p4() {
+    # creates dir on specified location and moves file
+    # TODO: CORRIGIR CRIAÇAO DA PASTA COM NOME CROPPADO
+    # mkdir "~/bmv2/examples/${$1%.*}"
+    cp $1 "~/bmv2/examples/$1/"
+
+    # runs the p4-code following p4pi pipeline
+    echo "Running $1 on bmv2 switch"
+    echo "$1" > t4p4s-switch
+    echo "Restarting bmv2.service"
+    systemctl restart bmv2.service
+    
+    # waits for the p4 to compile (p4pi does this for us)
+    sleep 2
+
+    if [ systemctl is-active --quiet "bmv2.service" ]; then
+        echo "bmv2 is running successfully!"
+    else
+        echo "Something went wrong!"
+        return 1
+    fi
+
+    return 0
+}
+
+# !IMPORTANT: connect to p4pi-middle wifi access-point
+function connect_to_wifi(){
+    local ssid="$1"
+    local password="$2"
+    nmcli radio wifi on #activate the wifi device
+    nmcli dev wifi connect $ssid password $password
+    if [ $? -ne 0 ]; then
+        echo "Failed to connect to WiFi access point: $ssid"
+        exit 1
+    else
+        echo "Connected to WiFi access point: $ssid"
+    fi
+}
+
+#Check if we are in root
+if [ "$(id -u)" -ne 0 ]; then
+    echo "You need to be in root to launch the entire script" >&2
+    exit 1
+fi
 
 #prerequisites
 apt-get install sshpass
 
-# !IMPORTANT: connect to p4pi-middle wifi access-point
-#nmcli radio wifi
-nmcli radio wifi on #activate the wifi device
-#nmcli dev wifi list
-nmcli dev wifi connect ${NAME_P4_MIDDLE} password ${ROOT_PASS}
-
 
 # connect to p4pi-middle and run port-to-port.p4 code
-sshpass -p "${ROOT_PASS}" ssh "${ROOT_USER}@${P4-MIDDLE}" << EOF
-    cd /
-    sudo ./port-to-port.p4
-    if [ \$? -eq 0 ]; then
+connect_to_wifi "$SSID_P4_MIDDLE" "$ROOT_PASS"
+sshpass -p "${ROOT_PASS}" ssh "${ROOT_USER}@${IP_P4_MIDDLE}" << EOF
+    run_p4 "port-to-port.p4"
+EOF
+
+# check if the code is running correctly (optional)
+sshpass -p "${ROOT_PASS}" ssh "${ROOT_USER}@${IP_P4_MIDDLE}" << EOF
+    pgrep -f port-to-port.p4 > /dev/null
+    if [ \$? -eq 0 ]; 
+    then
         echo "port-to-port.p4 is running."
     else
         echo "port-to-port.p4 is not running."
@@ -44,15 +87,12 @@ sshpass -p "${ROOT_PASS}" ssh "${ROOT_USER}@${P4-MIDDLE}" << EOF
     fi
 EOF
 
-# check if the code is running correctly (optional)
-
 # !IMPORTANT: connect to p4pi-end wifi access-point
-sudo -i
-${ADM_PASS}
 nmcli radio wifi on
 nmcli dev wifi connect ${NAME_P4_END} password ${ROOT_PASS}
 
 # connect to p4pi-end and check if there is internet access
+connect_to_wifi "$SSID_P4_END" "$ROOT_PASS"
 sshpass -p "${ROOT_PASS}" ssh "${ROOT_USER}@${P4_END}" << EOF
     ping ${INTERNET}
 
