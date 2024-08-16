56d332aa0946883d5ff14a829830a3cc8ee05f3c
julien_glmt <julien.guillemot156@gmail.com>
2024-08-16 11:39:40 -0300

    :zap: add comments and calcul_power.py

--- Power_cases/end/latency_end.py
+++ Power_cases/end/latency_end.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import sys, threading, argparse
+import psutil, time, os
 from time import sleep, monotonic
 from scapy.all import *
 from SX127x.LoRa import *
@@ -14,7 +15,12 @@ TREDBOLD = '\033[31;1m'
 TGREEN = '\033[32m'
 TYELLOW = '\033[33m'
 
+def timestamp():
+    """Returns the time elapsed since the program started."""
+    return f"[{monotonic() - lora.start_time:.4f}s] "
+
 def get_packet_size(min_value, max_value):
+    """Prompts the user to input the packet size within a specified range."""
     while True:
         try:
             user_input = int(input(f"Please choose the size of the packet in b (between {min_value} and {max_value}): "))
@@ -26,6 +32,7 @@ def get_packet_size(min_value, max_value):
             print("Error: Invalid input. Please enter a valid integer.")
 
 def get_repetitions():
+    """Prompts the user to input the number of repetitions."""
     while True:
         try:
             user_input = int(input("Please enter the number for repetitions: "))
@@ -36,42 +43,62 @@ def get_repetitions():
         except ValueError:
             print("Error: Invalid input. Please enter a valid integer.")
 
+def get_ttl(packet):
+    """Returns the TTL value from the IP layer of the packet, if it exists."""
+    if packet.haslayer(IP):
+        return packet[IP].ttl
+    else:
+        return None
+
 class Handler:
+    """Handles the main logic of sending packets, managing iterations, and tracking transmission times."""
     def __init__(self):
         self.tx_wait = 0
         self.end = False
         self.send = host == 'end'
         self.ack_received = False
         self.repetitions = get_repetitions()
-        self.packet_size = get_packet_size(64, 1500)  # Packet size
+        self.packet_size = get_packet_size(64, 1500)
+        self.iteration_start_time = None
         print(TYELLOW + f"Starting iteration with {self.repetitions} repetitions remaining.")
-        self.thread_started = False  # Ajout d'un flag pour g
+        self.thread_started = False
+        self.timer_started = False
+        self.iteration_times = []
 
     def end_test(self):
+        """Ends the current test iteration and resets for the next one if needed, or finishes all iterations."""
         self.repetitions -= 1
         if self.repetitions > 0:
             self.reset_for_next_iteration()
         else:
+            self.calculate_average_time()
             self.end = True
-            print("All iterations completed, exiting...")
+            print(timestamp() + f"All iterations completed, exiting...")
             sys.exit(0)
 
+    def calculate_average_time(self):
+        """Calculates and prints the average transmission time for all iterations."""
+        if self.iteration_times:
+            average_time = sum(self.iteration_times) / len(self.iteration_times)
+            print(TGREEN + timestamp() + f"Average transmission time: {average_time:.4f}s")
+        else:
+            print(TREDBOLD + timestamp() + "No iteration times recorded.")
+
     def reset_for_next_iteration(self):
+        """Resets the state for the next test iteration."""
         sleep(1)
-        print(TYELLOW + f"Starting iteration with {self.repetitions} repetitions remaining.")
+        print(TYELLOW + timestamp() +  f"{self.repetitions} repetitions remaining.")
         self.send = True
         self.tx_wait = 0
         self.ack_received = False
 
         self.reinitialize_lora_module()
 
-        #self.run()
+        self.iteration_start_time = monotonic()
 
     def reinitialize_lora_module(self):
-        print(TYELLOW + "Reinitializing LoRa module...")
+        """Reinitializes the LoRa module for the next iteration."""
         lora.set_mode(MODE.SLEEP)
-        sleep(0.2)  # Ensure the LoRa module has time to enter sleep mode
-
         lora.set_max_payload_length(128)
         lora.set_pa_config(pa_select=1)
         lora.set_bw(9)
@@ -79,39 +106,47 @@ class Handler:
         lora.reset_ptr_rx()
         lora.clear_irq_flags()
         lora.set_dio_mapping([0] * 6)
-        #sleep(2)
         lora.set_mode(MODE.RXCONT)
-        print("LoRa module reinitialized for next iteration.")
 
     def run(self):
-        while not self.end:
-            self.thread_started = True
-            if (not self.tx_wait) and self.send:
-                payload = 'z' * self.packet_size
-                data = bytes(Ether()/IP()/TCP()/payload)
-                packets = self.split(data)
+        """Main loop for sending packets, waiting for ACKs, and managing the test flow."""
+        self.iteration_start_time = monotonic()
 
-                lora.start_time = monotonic()
+        while not self.end:
+            if (not self.tx_wait) and self.send:
+                if not handler.timer_started:
+                    lora.start_time = monotonic()
+                    handler.timer_started = True
+
+                payload = 'z' * self.packet_size
+                data = Ether()/IP()/TCP()/payload
+
+                ttl = get_ttl(data)
+                if ttl is not None:
+                    print(f"TTL of the packet: {ttl}")
+
+                packets = self.split(bytes(data))
 
                 # Send the pieces one by one
                 for packet in packets:
-                    print(f"Sending packet fragment: {packet}")
+                    print(timestamp() + f"Sending packet fragment: {packet}")
                     lora.write_payload(list(packet))
                     lora.set_dio_mapping([1, 0, 0, 0, 0, 0])
                     lora.set_mode(MODE.TX)
                     self.tx_wait = 1
-                    sleep(0.5)  # Delay between transmissions
+                    sleep(0.5)
 
                 # Wait until ACK is received before proceeding
                 while not self.ack_received and not self.end:
                     sleep(1)
-                    print("Waiting for ACK...")
+                    print(timestamp() + "Waiting for ACK...")
 
             sleep(0.5)
-        print("exit")
+        print(timestamp() + f"exit")
         sys.exit()
 
     def split(self, data):
+        """Splits data into chunks that fit into the LoRa payload."""
         packets = []
         for i in range(0, len(data), 127):
             packet = data[i:i + 127]
@@ -130,21 +165,21 @@ class LoRaSocket(LoRa):
         self.end_time = 0
 
     def on_rx_done(self):
-        print("test")
+        """Handles the event when a packet is received."""
         try:
-            print("RX done, processing payload.")
             handler.tx_wait = 1
             payload = self.read_payload(nocheck=True)
             self.payload.extend(payload)
 
-            print(f"Received payload: {payload}")
-
             if len(payload) == 3 and ''.join([chr(x) for x in payload]) == "ACK":
-                print(TGREEN + "Received ACK!")
+                print(TGREEN + timestamp() + "Received ACK!")
                 self.end_time = monotonic()
-                total_time = self.end_time - self.start_time
-                total_time_ms = total_time * 1000
-                print(f"Total Transmission time: {total_time_ms:.4f} ms")
+
+                # Calculate iteration time
+                iteration_time = self.end_time - handler.iteration_start_time
+                print(timestamp() + f"Iteration Transmission time: {iteration_time:.4f}s")
+
+                handler.iteration_times.append(iteration_time)
                 handler.ack_received = True
                 handler.end_test()
 
@@ -153,21 +188,19 @@ class LoRaSocket(LoRa):
             if not handler.end:
                 lora.set_mode(MODE.RXCONT)
 
-#            current_mode = self.get_mode()
-#            print(f"Current LoRa mode after RX: {current_mode}")
-
         except OSError as e:
             print(f"Error in on_rx_done: {e}")
             handler.end_test()
 
     def on_tx_done(self):
+        """Handles the event when a packet has been successfully transmitted."""
         try:
-#            print("TX done, clearing IRQ flags and switching to RX mode.")
             self.clear_irq_flags(TxDone=1)
             self.set_dio_mapping([0] * 6)
-            lora.set_mode(MODE.RXCONT)
+            sleep(0.1)
+            self.set_mode(MODE.RXCONT)
             handler.tx_wait = 0
-            print(TYELLOW + "Send piece")
+            print(TYELLOW + timestamp() + "Send piece")
 
             if host == 'middle':
                 handler.end_test()
@@ -181,9 +214,13 @@ class LoRaSocket(LoRa):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--in", dest="pktin", default="wlan0", help="Sniffed Interface (packet in)", required=False)
+    parser.add_argument("-o", "--out", dest="pktout", default="lorarecv", help="Send Interface (packet out)", required=False)
     parser.add_argument("-m", "--mode", dest="mode", default="end", help="which host is running the code", required=False)
     parser.add_argument("-f", "--full-packet", dest="full_packet", help="Send full packet or only 127B", action='store_true')
     args = parser.parse_args()
+    pktin = args.pktin
+    pktout = args.pktout
     host = args.mode
     full_packet = args.full_packet
     send = (host == 'end')
@@ -198,7 +235,7 @@ if __name__ == '__main__':
 
     try:
         while True:
-            sleep(0.1)  # Small delay to allow other threads to work
+            sleep(0.1)
             if handler.end:
                 break
     finally:
--- Power_cases/middle/latency_middle.py
+++ Power_cases/middle/latency_middle.py
@@ -15,6 +15,10 @@ TREDBOLD =  '\033[31;1m'
 TGREEN =  '\033[32m'
 TYELLOW =  '\033[33m'
 
+def timestamp():
+    """Returns the time elapsed since the program started."""
+    return f"[{monotonic() - lora.start_time:.4f}s] "
+
 class Handler:
     def __init__(self):
         self.tx_wait = 0
@@ -23,38 +27,53 @@ class Handler:
         self.packet_received = False
         self.timer_started = False
         self.packet_counter = 0  # Initialize a packet counter
+        self.iteration_start_time = False
 
     def end_test(self):
+        """Ends the current test and sets the end flag."""
         self.end = True
 
     def switch_mode(self):
+        """Switches between send and receive modes."""
         self.send = not self.send
 
     def run(self):
+        """Main loop for sending packets, waiting for ACKs, and managing the test flow."""
         while not self.end:
             if (not self.tx_wait) and self.send:
-                # creates packet with xB
-                payload = 'z' * 1500
-                data = bytes(Ether()/IP()/TCP()/payload)
-                packets = self.split(data)
+                if not handler.timer_started:
+                    lora.start_time = monotonic()
+                    handler.timer_started = True
 
-                # sends the pieces one by one
+                payload = 'z' * self.packet_size
+                data = Ether()/IP()/TCP()/payload
+
+                ttl = get_ttl(data)
+                if ttl is not None:
+                    print(f"TTL of the packet: {ttl}")
+
+                packets = self.split(bytes(data))
+
+                # Send the pieces one by one
                 for packet in packets:
+                    print(timestamp() + f"Sending packet fragment: {packet}")
                     lora.write_payload(list(packet))
-                    lora.set_dio_mapping([1,0,0,0,0,0]) # set DIO0 for txdone
+                    lora.set_dio_mapping([1, 0, 0, 0, 0, 0])
                     lora.set_mode(MODE.TX)
                     self.tx_wait = 1
                     sleep(0.5)
 
-                    if not full_packet:
-                        self.switch_mode()
-                        break
+                # Wait until ACK is received before proceeding
+                while not self.ack_received and not self.end:
+                    sleep(1)
+                    print(timestamp() + "Waiting for ACK...")
 
             sleep(0.5)
-        print("exit")
+        print(timestamp() + "exit")
         sys.exit()
 
     def split(self, data):
+        """Splits data into chunks that fit into the LoRa payload."""
         packets = []
         for i in range(0, len(data), 127):
             packet = data[i:i + 127]
@@ -73,40 +92,39 @@ class LoRaSocket(LoRa):
         self.start_time = 0
         self.end_time = 0
 
-    # when LoRa receives data send to socket conn
     def on_rx_done(self):
+        """Handles the event when a packet is received."""
         try:
             if not handler.timer_started:
                 lora.start_time = monotonic()
                 handler.timer_started = True
+            handler.iteration_start_time = True
             handler.tx_wait = 1
             payload = self.read_payload(nocheck=True)
             self.payload.extend(payload)
-
             print(TGREEN + "Received piece!")
 
             if full_packet:
                 if len(payload) != 127:
                     if len(self.payload) > 34:
-                        print(TGREEN + "Full packet received")
+                        handler.packet_counter += 1
+                        print(TGREEN + timestamp() + f"Full packet {handler.packet_counter} received")
                         if host == 'middle':
                             self.end_time = monotonic() #record the end time
-                            total_time = self.end_time - self.start_time
-                            total_time_ms = total_time  * 1000
-                            print(TREDBOLD + f"Paquet transmission time before ACK: {total_time_ms:.4f} ms")
+                            packet_time = self.end_time - handler.iteration_start_time
+                            total_time = self.end_time - handler.iteration_start_time
+                            print(TREDBOLD + timestamp() + f"Paquet transmission time before ACK {handler.packet_counter}: {total_time:.4f}s")
 
                             # Send ACK back to end
                             sleep(1)
-                            print("test")
                             ack_packet = "ACK".encode()
                             lora.write_payload(list(ack_packet))
                             lora.set_dio_mapping([1, 0, 0, 0, 0, 0])  # Set DIO0 for txdone
                             lora.set_mode(MODE.TX)
-                            print(TYELLOW + "Send ACK to raspi end")
+                            print(TYELLOW + timestamp() + f"Send ACK {handler.packet_counter} to raspi end")
 
                             self.payload = []  # Clear the payload after sending ACK
                             handler.tx_wait = 0
-#                            handler.switch_mode()
                             handler.packet_received = True  # Mark packet as received
 
             else:
@@ -124,26 +142,28 @@ class LoRaSocket(LoRa):
 
     # after data sent by LoRa reset to receive mode
     def on_tx_done(self):
+        """Handles the event when a packet has been successfully transmitted."""
         try:
             self.clear_irq_flags(TxDone=1)
             self.set_dio_mapping([0] * 6)
             self.set_mode(MODE.RXCONT)
             handler.tx_wait = 0
-
-            print(TYELLOW + "Switched to RX mode after TX (middle)")
-
-            handler.packet_received = False
+            handler.packet_received = False  # Reset for the next packet
 
         except OSError as e:
             print(f"Error in on_tx_done: {e}")
             handler.end_test()
 
-
 if __name__ == '__main__':
+    #./transceiver.py -i INTERFACE_IN -o INTERFACE_OUT -m mode -f full_packet
     parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--in", dest="pktin", default="int0", help="Sniffed Interface (packet in)", required=False)
+    parser.add_argument("-o", "--out", dest="pktout", default="lorarecv", help="Send Interface (packet out)", required=False)
     parser.add_argument("-m", "--mode", dest="mode", default="end", help="which host is running the code", required=False)
     parser.add_argument("-f", "--full-packet", dest="full_packet", help="Send full packet or only 127B", action='store_true')
     args = parser.parse_args()
+    pktin = args.pktin
+    pktout = args.pktout
     host = args.mode
     full_packet = args.full_packet
     send = (host == 'end')
